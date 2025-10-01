68e4b069667c2855c60d275f68e3b2d69dd998eb
Sagar Sen <sagarsen27842@gmail.com>
2025-10-01 13:40:33 +0530

    values in go

---
+++ 2_values/main.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+	//Define values
+
+	// int
+	fmt.Println(1 + 1)
+
+	// string
+	fmt.Println("Hello" + " " + "World")
+
+	// bool
+	fmt.Println(true)
+	fmt.Println(!true)
+
+	// float
+	fmt.Println(1.5 + 2 + 2)
+
+	// complex
+	fmt.Println(complex(1, 2) + complex(3, 4))
+
+	// division
+	fmt.Println(5 / 2)
+
+	// multiplication
+	fmt.Println(5 * 2)
+
+}
